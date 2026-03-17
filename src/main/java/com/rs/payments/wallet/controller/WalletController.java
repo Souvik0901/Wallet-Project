f35f9b5014e8f9251d8ff2e358694b8b6fdf7c11
Souvik <souviksen147@gmail.com>
2026-03-18 00:17:47 +0530

    Deposit Feature Completed

--- src/main/java/com/rs/payments/wallet/controller/WalletController.java
+++ src/main/java/com/rs/payments/wallet/controller/WalletController.java
@@ -1,6 +1,7 @@
 package com.rs.payments.wallet.controller;
 
 import com.rs.payments.wallet.dto.CreateWalletRequest;
+import com.rs.payments.wallet.dto.DepositRequest;
 import com.rs.payments.wallet.model.Wallet;
 import com.rs.payments.wallet.service.WalletService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,7 +10,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,4 +51,13 @@ public class WalletController {
         Wallet wallet = walletService.createWalletForUser(request.getUserId());
         return ResponseEntity.ok(wallet);
     }
+
+    @PostMapping("/{id}/deposit")
+     public ResponseEntity<Wallet> deposit(
+                @PathVariable UUID id,
+                @Valid @RequestBody DepositRequest request) {
+
+        Wallet wallet = walletService.deposit(id, request.getAmount());
+        return ResponseEntity.ok(wallet);
+     }
 }
